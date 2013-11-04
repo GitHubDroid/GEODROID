b5e00128898405a1b4cb472e7f4f68cf1fd7c41a
GitHubDroid <promptnet@promptnetworks.com>
2013-11-04 17:04:50 -0500

    add end marker also

--- GeoDroid/src/eu/hydrologis/geodroid/maps/overlays/GeopaparazziOverlay.java
+++ GeoDroid/src/eu/hydrologis/geodroid/maps/overlays/GeopaparazziOverlay.java
@@ -313,9 +313,12 @@ public abstract class GeopaparazziOverlay extends Overlay {
              int x = overlayWay.cachedWayPositions[i][0].x - drawPosition.x;
              int y = overlayWay.cachedWayPositions[i][0].y - drawPosition.y;
              this.wayPath.moveTo(x, y);
+             int lastX = 0;
+             int lastY = 0;
              for( int j = 1; j < overlayWay.cachedWayPositions[i].length; ++j ) {
-                this.wayPath.lineTo(overlayWay.cachedWayPositions[i][j].x - drawPosition.x, overlayWay.cachedWayPositions[i][j].y
-                        - drawPosition.y);
+               lastX = overlayWay.cachedWayPositions[i][j].x - drawPosition.x;
+               lastY = overlayWay.cachedWayPositions[i][j].y - drawPosition.y;
+               this.wayPath.lineTo(lastX, lastY);
             }
             
             // draw start points
@@ -337,7 +340,9 @@ public abstract class GeopaparazziOverlay extends Overlay {
                      wayStartPaintFill.setColor(defaultWayPaintFill.getColor());
                  }
              }
-             canvas.drawCircle(x, y, size * 2, wayStartPaintFill);
+             size = size * 2;
+             canvas.drawCircle(lastX, lastY, size, wayStartPaintFill);
+             canvas.drawRect(x - size, y - size, x + size, y + size, wayStartPaintFill);
         }
     
         // draw them
